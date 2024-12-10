47d0d848299db69fee262b9f7daac0424b76e41f
Konrad Hinsen <konrad.hinsen@fastmail.net>
2024-12-10 13:40:13 +0100

    Directory of Scriba files

--- hyperdoc.lisp
+++ hyperdoc.lisp
@@ -14,20 +14,42 @@
    (pages :initform nil)))
 
 (defclass hyperdoc-page ()
-  ((file :initarg :file)))
+  ((file :initarg :file)
+   (document :initarg :document)))
 
 (defun make-hyperdoc (directory)
   (let ((hd (make-instance 'hyperdoc :directory directory)))
-    (dolist (page-file (uiop:directory-files directory))
-      (let ((page (make-instance 'hyperdoc-page :file page-file)))
-        (push page (slot-value hd 'pages))))
+    (let ((common-doc.file:*base-directory* directory))
+      (dolist (page-file (uiop:directory-files directory))
+        (let ((page (make-page page-file)))
+          (push page (slot-value hd 'pages)))))
     hd))
+
+(defun make-page (file)
+  (let ((document (common-doc.format:parse-document (make-instance 'scriba:scriba) file)))
+    (make-instance 'hyperdoc-page
+                   :file file
+                   :document document)))
 
 (defview 👀items (hd hyperdoc)
   (with-slots (pages) hd
     (-> pages
       👀items
       (rename :title "Pages" :priority 1))))
+
+(defmethod text-representation ((page hyperdoc-page))
+  (common-doc:title (slot-value page 'document)))
+
+(defview 👀content (page hyperdoc-page)
+  (-> page
+    (slot-value 'document)
+    👀content))
+
+(defview 👀source (page hyperdoc-page)
+  (-> page
+    (slot-value 'file)
+    👀content
+    (rename :title "Source" :priority 3)))
 
 (defvar *doc*
   (make-hyperdoc (asdf:system-relative-pathname
