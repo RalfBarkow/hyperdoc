cba8219e79006b140ac921b73de320172ec09e72
Konrad Hinsen <konrad.hinsen@fastmail.net>
2025-12-18 18:15:51 +0100

    Improved backlink view

--- hyperbook-explorer/link-views.lisp
+++ hyperbook-explorer/link-views.lisp
@@ -6,79 +6,79 @@
 
 (views:defview 👀links (page page)
   (when-let (links (links-of page))
+    (views:html-view :title "Links" :priority 5
+      (views:add-asset-path "/hyperdoc/"
+                            (asdf:system-relative-pathname
+                             :hyperdoc
+                             "assets/hyperbook/"))
+      (views:include-css "/hyperbook/css/hyperbook.css")
+      (views:html
+        (:div :class "hyperbook-page"
+              (when-let (links (page-links-of links))
+                (views:html
+                  (:h2 (views:esc "Pages")))
+                (page-link-section
+                 (mapcar #'(lambda (link)
+                             (-> link thunk-of views:eval-thunk))
+                         links)))
+              (when-let (links (hyperbook-links-of links))
+                (views:html
+                  (:h2 (views:esc "HyperDocs")))
+                (views:html-table (mapcar #'(lambda (link)
+                                              (-> link thunk-of views:eval-thunk))
+                                          links)))
+              (when-let (links (web-links-of links))
+                (views:html
+                  (:h2 (views:esc "Web links"))
+                  (:table :class "inspector-table"
+                    (dolist (link (mapcar #'url-of links))
+                      (views:html
+                        (:tr (:td (:a :href link :target "_blank"
+                                      (views:esc link)))))))))))
+      (unless (and links
+                   (or (page-links-of links)
+                       (hyperbook-links-of links)
+                       (web-links-of links)))
+        (views:html (views:esc "None"))))
     (link-view links)))
 
 (views:defview 👀backlinks (page page)
-  (let* ((pages (find-backlink-sources (-> page hyperbook-of id-of)
-                                       (-> page id-of)))
-         (page-links (mapcar #'(lambda (page)
-                                 (make-page-link page
-                                                 (-> page hyperbook-of id-of)
-                                                 (-> page id-of)))
-                             pages))
-         (links (make-instance 'links :page-links page-links)))
-    (-> links
-        link-view
-        (views:rename :title "Backlinks" :priority 6))))
-
-(defun link-view (links)
-  (views:html-view :title "Links" :priority 5
+  (views:html-view :title "Backlinks" :priority 6
     (views:add-asset-path "/hyperdoc/"
                           (asdf:system-relative-pathname
                            :hyperdoc
                            "assets/hyperbook/"))
     (views:include-css "/hyperbook/css/hyperbook.css")
-    (views:html
-      (:div :class "hyperbook-page"
-            (link-view-section :page (page-links-of links))
-            (link-view-section :hyperdoc (hyperbook-links-of links))
-            (link-view-section :web (web-links-of links))))
-    (unless (and links
-                 (or (page-links-of links)
-                     (hyperbook-links-of links)
-                     (web-links-of links)))
-      (views:html (views:esc "None"))))  )
+    (let* ((pages (find-backlink-sources (-> page hyperbook-of id-of)
+                                         (-> page id-of)))
+           (page-links (mapcar #'(lambda (page)
+                                   (make-page-link page
+                                                   (-> page hyperbook-of id-of)
+                                                   (-> page id-of)))
+                               pages)))
+      (views:html
+        (:div :class "hyperbook-page"
+              (if page-links
+                  (link-view-section :page page-links)
+                  (views:html (views:esc "None"))))))))
 
-(defgeneric link-view-section (kind links))
+(defmethod page-link-section (pages)
+  (let ((by-hyperdoc (make-hash-table))
+        lookup-failures)
+    (dolist (page pages)
+      (if (typep page 'page)
+          (let ((hd (-> page hyperbook-of)))
+            (alexandria:ensure-gethash hd by-hyperdoc nil)
+            (pushnew page (gethash hd by-hyperdoc)))
+          (pushnew page lookup-failures)))
+    (loop for hd being the hash-keys of by-hyperdoc
+            using (hash-value pages)
+          do (views:html
+               (:table :class "inspector-table"
+                 (:tr (:td (:i (views:object-ref hd))))
+                 (:tr (:td (views:html-table pages))))))
+    (when lookup-failures
+      (views:html
+        (:h4 "Bad links")
+        (views:html-table lookup-failures)))))
 
-(defmethod link-view-section ((kind (eql :page)) links)
-  (when links
-    (views:html
-      (:h2 (views:esc "Pages"))
-      (let ((by-hyperdoc (make-hash-table))
-            lookup-failures)
-        (dolist (page-link links)
-          (let ((page (-> page-link thunk-of views:eval-thunk)))
-            (if (typep page 'page)
-                (let ((hd (-> page hyperbook-of)))
-                  (alexandria:ensure-gethash hd by-hyperdoc nil)
-                  (pushnew page (gethash hd by-hyperdoc)))
-                (pushnew page lookup-failures))))
-        (loop for hd being the hash-keys of by-hyperdoc
-                using (hash-value pages)
-              do (views:html
-                   (:table :class "inspector-table"
-                     (:tr (:td (:i (views:object-ref hd))))
-                     (:tr (:td (views:html-table pages))))))
-        (when lookup-failures
-          (views:html
-            (:h4 "Bad links")
-            (views:html-table lookup-failures)))))))
-
-(defmethod link-view-section ((kind (eql :hyperdoc)) links)
-  (when links
-    (views:html
-      (:h2 (views:esc "HyperDocs"))
-      (views:html-table (mapcar #'(lambda (l)
-                                    (-> l thunk-of views:eval-thunk))
-                                links)))))
-
-(defmethod link-view-section ((kind (eql :web)) links)
-  (when links
-    (views:html
-      (:h2 (views:esc "Web links"))
-      (:table :class "inspector-table"
-        (dolist (link (mapcar #'url-of links))
-          (views:html
-            (:tr (:td (:a :href link :target "_blank"
-                          (views:esc link))))))))))
