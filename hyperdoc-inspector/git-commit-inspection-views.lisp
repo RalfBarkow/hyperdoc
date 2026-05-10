7a3815a72f710da4b82636ef85e47394bac07643
rgb <ralf.barkow@me.com>
2026-05-10 16:28:22 +0200

    Make Git commit changed files inspectable

--- hyperdoc-inspector/git-commit-inspection-views.lisp
+++ hyperdoc-inspector/git-commit-inspection-views.lisp
@@ -21,6 +21,22 @@
                   (min 12
                        (length (hyperdoc::git-commit-hash-of commit))))))
 
+(defmethod html-inspector-views:text-representation
+    ((file hyperdoc::git-file-at-commit))
+  (format nil "~A @ ~A"
+          (hyperdoc::git-file-path-of file)
+          (subseq
+           (hyperdoc::git-commit-hash-of
+            (hyperdoc::git-file-commit-of file))
+           0
+           12)))
+
+(defmethod html-inspector-views:text-representation
+    ((change hyperdoc::git-commit-file-change))
+  (format nil "~A ~A"
+          (hyperdoc::git-commit-file-change-status-of change)
+          (hyperdoc::git-commit-file-change-path-of change)))
+
 (html-inspector-views:defview 👀commit (commit hyperdoc::git-commit)
   (html-inspector-views:html-view :title "Commit" :priority 1
     (handler-case
@@ -57,9 +73,36 @@
 (html-inspector-views:defview 👀changed-files (commit hyperdoc::git-commit)
   (html-inspector-views:html-view :title "Changed files" :priority 4
     (handler-case
-        (render-git-pre
-         (format nil "~{~A~%~}"
-                 (hyperdoc::git-commit-changed-files commit)))
+        (html-inspector-views:html
+          (:table :class "inspector-table"
+                  (:tr
+                   (:th (html-inspector-views:esc "Status"))
+                   (:th (html-inspector-views:esc "File"))
+                   (:th (html-inspector-views:esc "Previous path")))
+                  (dolist (change
+                            (hyperdoc::git-commit-file-changes commit))
+                    (html-inspector-views:html
+                      (:tr
+                       (:td
+                        (:tt
+                         (html-inspector-views:esc
+                          (hyperdoc::git-commit-file-change-status-of
+                           change))))
+                       (:td
+                        (html-inspector-views:object-ref
+                         (hyperdoc::git-commit-file-change-file change)
+                         :display
+                         (hyperdoc::git-commit-file-change-path-of
+                          change)))
+                       (:td
+                        (let ((old-path
+                                (hyperdoc::git-commit-file-change-old-path-of
+                                 change)))
+                          (if old-path
+                              (html-inspector-views:html
+                                (:code
+                                 (html-inspector-views:esc old-path)))
+                              (html-inspector-views:esc "")))))))))
       (condition (condition)
         (render-git-error condition)))))
 
@@ -68,5 +111,35 @@
     (handler-case
         (render-git-pre
          (hyperdoc::git-commit-patch commit))
+      (condition (condition)
+        (render-git-error condition)))))
+
+(html-inspector-views:defview 👀overview (file hyperdoc::git-file-at-commit)
+  (html-inspector-views:html-view :title "Overview" :priority 1
+    (html-inspector-views:html
+      (:table :class "inspector-table"
+              (:tr
+               (:td (html-inspector-views:esc "Commit"))
+               (:td
+                (html-inspector-views:object-ref
+                 (hyperdoc::git-file-commit-of file))))
+              (:tr
+               (:td (html-inspector-views:esc "Path"))
+               (:td
+                (:code
+                 (html-inspector-views:esc
+                  (hyperdoc::git-file-path-of file)))))
+              (:tr
+               (:td (html-inspector-views:esc "Blob spec"))
+               (:td
+                (:code
+                 (html-inspector-views:esc
+                  (hyperdoc::git-file-blob-spec file)))))))))
+
+(html-inspector-views:defview 👀contents (file hyperdoc::git-file-at-commit)
+  (html-inspector-views:html-view :title "Contents" :priority 2
+    (handler-case
+        (render-git-pre
+         (hyperdoc::git-file-contents file))
       (condition (condition)
         (render-git-error condition)))))
--- hyperdoc/git-commit-inspection.lisp
+++ hyperdoc/git-commit-inspection.lisp
@@ -107,3 +107,102 @@
     (REMOVE "" (UIOP/UTILITY:SPLIT-STRING OUTPUT :SEPARATOR '(#\Newline)) :TEST
             #'STRING=)))
 
+
+
+;;;; Inspectable files changed by a Git commit.
+
+(defclass git-file-at-commit ()
+  ((commit
+    :reader git-file-commit-of
+    :initarg :commit)
+   (path
+    :reader git-file-path-of
+    :initarg :path
+    :type string))
+  (:documentation
+   "A repository file path as seen at a particular Git commit."))
+
+(defmethod print-object ((file git-file-at-commit) stream)
+  (print-unreadable-object (file stream :type t :identity nil)
+    (format stream "~A @ ~A"
+            (git-file-path-of file)
+            (subseq (git-commit-hash-of (git-file-commit-of file))
+                    0
+                    (min 12
+                         (length
+                          (git-commit-hash-of
+                           (git-file-commit-of file))))))))
+
+(defclass git-commit-file-change ()
+  ((commit
+    :reader git-commit-file-change-commit-of
+    :initarg :commit)
+   (status
+    :reader git-commit-file-change-status-of
+    :initarg :status
+    :type string)
+   (path
+    :reader git-commit-file-change-path-of
+    :initarg :path
+    :type string)
+   (old-path
+    :reader git-commit-file-change-old-path-of
+    :initarg :old-path
+    :initform nil
+    :type (or null string)))
+  (:documentation
+   "One file-level name-status row in a Git commit."))
+
+(defmethod print-object ((change git-commit-file-change) stream)
+  (print-unreadable-object (change stream :type t :identity nil)
+    (format stream "~A ~A"
+            (git-commit-file-change-status-of change)
+            (git-commit-file-change-path-of change))))
+
+(defun git-status-rename-or-copy-p (status)
+  (and (< 0 (length status))
+       (find (char status 0) "RC" :test #'char=)))
+
+(defun git-commit-file-change-from-line (commit line)
+  "Parse one git show --name-status row into a GIT-COMMIT-FILE-CHANGE."
+  (let* ((parts (uiop:split-string line :separator '(#\Tab)))
+         (status (or (first parts) ""))
+         (rename-or-copy? (git-status-rename-or-copy-p status))
+         (old-path (and rename-or-copy? (second parts)))
+         (path (if rename-or-copy?
+                   (third parts)
+                   (second parts))))
+    (make-instance 'git-commit-file-change
+                   :commit commit
+                   :status status
+                   :old-path old-path
+                   :path (or path ""))))
+
+(defun git-commit-file-changes (commit)
+  "Return inspectable file-change objects for COMMIT."
+  (mapcar (lambda (line)
+            (git-commit-file-change-from-line commit line))
+          (git-commit-changed-files commit)))
+
+(defun git-commit-file-change-file (change)
+  "Return the changed path as an inspectable file-at-commit object."
+  (make-instance 'git-file-at-commit
+                 :commit (git-commit-file-change-commit-of change)
+                 :path (git-commit-file-change-path-of change)))
+
+(defun git-file-blob-spec (file)
+  (format nil "~A:~A"
+          (git-commit-hash-of (git-file-commit-of file))
+          (git-file-path-of file)))
+
+(defun git-file-contents (file)
+  "Return the contents of FILE at its commit.
+
+Deleted paths may not have a blob at the commit itself; callers should handle
+GIT-COMMAND-FAILED."
+  (git-run-string
+   (git-repository-root-of
+    (git-commit-repository-of
+     (git-file-commit-of file)))
+   "show" "--no-color"
+   (git-file-blob-spec file)))
