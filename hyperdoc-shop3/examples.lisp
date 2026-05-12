eeee56bef898b24c22b9d40320a286e71a84dfa7
Ralf Barkow <ralf.barkow@me.com>
2026-05-12 09:51:05 +0200

    shop3: add ASDF refactor maintenance domain

--- hyperdoc-shop3/examples.lisp
+++ hyperdoc-shop3/examples.lisp
@@ -3,3 +3,23 @@
 ;;;; Copyright (c) 2026
 
 (in-package #:hyperdoc/shop3)
+
+(defparameter *hyperdoc-asdf-refactor-checklist-fixture-pathname*
+  (asdf:system-relative-pathname
+   :hyperdoc
+   "hyperdoc-shop3-asdf-refactor-plan.sexp"))
+
+(defun read-hyperdoc-asdf-refactor-checklist-fixture
+    (&optional (pathname *hyperdoc-asdf-refactor-checklist-fixture-pathname*))
+  (with-open-file (stream pathname :direction :input)
+    (let ((*package* (find-package '#:hyperdoc/shop3)))
+      (read stream nil nil))))
+
+(defun hyperdoc-asdf-refactor-checklist-example ()
+  (let* ((plan-result (run-hyperdoc-asdf-refactor-plan-object))
+         (computed-checklist (hyperdoc-plan-checklist plan-result))
+         (fixture-checklist (read-hyperdoc-asdf-refactor-checklist-fixture)))
+    (list :matches-fixture (equal computed-checklist fixture-checklist)
+          :computed-checklist computed-checklist
+          :fixture-checklist fixture-checklist
+          :summary (hyperdoc-plan-summary plan-result))))
