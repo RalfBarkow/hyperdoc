e806d7609c85fe103f44262a63ed4a026ba5ce6f
rgb <ralf.barkow@me.com>
2026-08-04 19:45:59 +0200

    feat(dev): add Nix-provided SLY launcher

--- flake.nix
+++ flake.nix
@@ -3,26 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
     html-inspector-views = {
       url = "git+https://codeberg.org/khinsen/html-inspector-views.git";
       flake = false;
     };
+
     plump-inspector-views = {
       url = "git+https://codeberg.org/khinsen/plump-inspector-views.git";
       flake = false;
     };
+
     clog-moldable-inspector = {
       url = "git+https://codeberg.org/khinsen/clog-moldable-inspector.git";
       flake = false;
     };
+
     lwcells = {
       url = "github:kchanqvq/lwcells";
       flake = false;
     };
+
     named-closure = {
       url = "github:BlueFlo0d/named-closure";
       flake = false;
     };
+
     njson = {
       url = "github:atlas-engineer/njson";
       flake = false;
@@ -46,57 +52,97 @@
         "x86_64-darwin"
         "x86_64-linux"
       ];
+
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in {
-      devShells = forAllSystems (system:
+      devShells = forAllSystems (
+        system:
         let
-          pkgs = import nixpkgs { inherit system; };
-          sbcl = pkgs.sbcl.withPackages (ps: with ps; [
-            alexandria
-            ps.arrow-macros
-            babel
-            bordeaux-threads
-            cffi
-            ps."cl-base32"
-            ps."cl-base64"
-            ps."cl-slug"
-            ps."clack-handler-hunchentoot"
-            ps."clog-ace"
-            cl-who
-            clog
-            ps."closer-mop"
-            dissect
-            ps."damn-fast-stable-priority-queue"
-            drakma
-            ps."eclector-concrete-syntax-tree"
-            flexi-streams
-            fset
-            ps."local-time"
-            lquery
-            iterate
-            jzon
-            plump
-            puri
-            ps."s-graphviz"
-            serapeum
-            sha1
-            shasht
-            str
-            swank
-            ps."trivial-clipboard"
-            ps."trivial-cltl2"
-            ps."trivial-package-local-nicknames"
-            usocket
-            ps._3bmd
-            ps._3bmd-ext-code-blocks
-          ]);
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          sbcl = pkgs.sbcl.withPackages (
+            ps:
+            with ps; [
+              alexandria
+              ps.arrow-macros
+              babel
+              bordeaux-threads
+              cffi
+              ps."cl-base32"
+              ps."cl-base64"
+              ps."cl-slug"
+              ps."clack-handler-hunchentoot"
+              ps."clog-ace"
+              cl-who
+              clog
+              ps."closer-mop"
+              dissect
+              ps."damn-fast-stable-priority-queue"
+              drakma
+              ps."eclector-concrete-syntax-tree"
+              flexi-streams
+              fset
+              ps."local-time"
+              lquery
+              iterate
+              jzon
+              plump
+              puri
+              ps."s-graphviz"
+              serapeum
+              sha1
+              shasht
+              str
+              swank
+              ps."trivial-clipboard"
+              ps."trivial-cltl2"
+              ps."trivial-package-local-nicknames"
+              usocket
+              ps._3bmd
+              ps._3bmd-ext-code-blocks
+            ]
+          );
+
+          emacsPackages =
+            pkgs.emacsPackagesFor pkgs.emacs;
+
+          hyperdocEmacs =
+            emacsPackages.emacsWithPackages (
+              epkgs: [
+                epkgs.sly
+              ]
+            );
+
+          hyperdocSly =
+            pkgs.writeShellApplication {
+              name = "hyperdoc-sly";
+
+              runtimeInputs = [
+                pkgs.git
+                sbcl
+                hyperdocEmacs
+              ];
+
+              text =
+                builtins.readFile
+                  ./scripts/hyperdoc-sly.sh;
+            };
         in {
           default = pkgs.mkShell {
-            packages = [ pkgs.git sbcl ];
+            packages = [
+              pkgs.git
+              sbcl
+              hyperdocEmacs
+              hyperdocSly
+            ];
+
             shellHook = ''
               export CL_SOURCE_REGISTRY="${clog-moldable-inspector}//:${html-inspector-views}//:${plump-inspector-views}//:${lwcells}//:${named-closure}//:${njson}//:$PWD//"
             '';
           };
-        });
+        }
+      );
     };
 }
